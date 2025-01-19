0881a3c790c1787dfbd803baf3ff306cb02f8f20
Maggie Jiu <108047619+Ephernicus@users.noreply.github.com>
2025-01-19 12:46:03 -0800

    added working but unpolished menus to mode a and b

--- landing.js
+++ landing.js
@@ -1,15 +1,31 @@
-// landing page buttons
+// landing page constants
 const modeAButton = document.getElementById('modeAButton');
 const modeBButton = document.getElementById('modeBButton');
+const modeButtons = document.getElementById('landing-page');
+const selectionMenuA = document.getElementById('selection-menu-a');
+const selectionMenuB = document.getElementById('selection-menu-b');
+const modeAStart = document.getElementById('begin-button-a');
+const modeBStart = document.getElementById('begin-button-b');
 
-// navigate to mode A 
+// open mode A selection menu
 modeAButton.addEventListener('click', () => {
+    modeButtons.style.display = 'none'; // Hide mode buttons
+    selectionMenuA.style.display = 'block'; // Show selection menu A
+});
+// open mode B selection menu
+modeBButton.addEventListener('click', () => {
+    modeButtons.style.display = 'none'; // Hide mode buttons
+    selectionMenuB.style.display = 'block'; // Show selection menu B
+});
+
+// navigate to mode A
+modeAStart.addEventListener('click', () => {
     console.log('button');
     window.location.href = 'modeA.html'; 
 });
-
 // navigate to mode B
-modeBButton.addEventListener('click', () => {
+modeBStart.addEventListener('click', () => {
     window.location.href = 'modeB.html'; 
 });
+
 
